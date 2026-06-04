b8424147ed6ac2ac2dded2a87463d6c8c3d11317
tgyeongt <tgyeongt@gmail.com>
2026-06-04 09:34:26 +0900

    Fix: background color 수정

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,9 +1,9 @@
-import LoginForm from './ui/LoginForm'
+import LoginForm from "./ui/LoginForm";
 
 export default function LoginPage() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-gray-50">
+    <div className="flex min-h-screen items-center justify-center bg-[#F8FAFC]">
       <LoginForm />
     </div>
-  )
+  );
 }
